6b8adf425cf7b9d15e5600d3e01d9769ae4a6f30
Prime-Cave <tomisin.akinfemiwa@gmail.com>
2025-03-23 03:05:29 +0200

    updates

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,21 @@
 package controllers
+
+import(
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"go/bookstore/pkg/utils"
+	"go/bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request){
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
